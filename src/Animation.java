f27bc218ce2647866f58d4c41c798d61dbc8ca63
Squid49134 <ransom@Udel.edu>
2019-04-03 19:12:34 -0400

    changed relative file paths of images to absolute paths so the program can be run from the cmd line rather than just in eclipse

--- src/Animation.java
+++ src/Animation.java
@@ -57,7 +57,7 @@ public class Animation extends JPanel implements Serializable{
        	screenHeight = (int) screenSize.getHeight();
 		images = new ArrayList<AniObject>();
 		try {
-			BufferedImage beach = ImageIO.read(new File("images/beach5.png"));
+			BufferedImage beach = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/beach5.png"));
 			List<BufferedImage> beachList = new ArrayList<BufferedImage>();
 			beachList.add(beach);
 			images.add(new AniObject("beach", 0, 0, (int) ((3./4.) * screenWidth), screenHeight, beachList));
@@ -66,7 +66,7 @@ public class Animation extends JPanel implements Serializable{
 		}
 
 		try {
-			BufferedImage board = ImageIO.read(new File("images/board.png"));
+			BufferedImage board = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/board.png"));
 			List<BufferedImage> boardList = new ArrayList<BufferedImage>();
 			boardList.add(board);
 			// screen ratio is used for determining the size and position of various images
@@ -79,7 +79,7 @@ public class Animation extends JPanel implements Serializable{
 		}
 		
 		try {
-			BufferedImage grass = ImageIO.read(new File("images/grass.png"));
+			BufferedImage grass = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/grass.png"));
 			List<BufferedImage> grassList = new ArrayList<BufferedImage>();
 			grassList.add(grass);
 			// each grass image is a separate object with its own position and size
@@ -111,7 +111,7 @@ public class Animation extends JPanel implements Serializable{
 		if (num == 1) {
 			BufferedImage beach1 = null;
 			try {
-				beach1 = ImageIO.read(new File("images/beach1.png"));
+				beach1 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/beach1.png"));
 			} catch (IOException e) {
 				System.out.println("Failed to load beach, trying bin folder");
 			}
@@ -120,7 +120,7 @@ public class Animation extends JPanel implements Serializable{
 		else if (num == 2) {
 			BufferedImage beach2 = null;
 			try {
-				beach2 = ImageIO.read(new File("images/beach2.png"));
+				beach2 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/beach2.png"));
 			} catch (IOException e) {
 				System.out.println("Failed to load beach, trying bin folder");
 			}
@@ -129,7 +129,7 @@ public class Animation extends JPanel implements Serializable{
 		else if (num == 3) {
 			BufferedImage beach3 = null;
 			try {
-				beach3 = ImageIO.read(new File("images/beach3.png"));
+				beach3 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/beach3.png"));
 			} catch (IOException e) {
 				System.out.println("Failed to load beach, trying bin folder");
 			}
@@ -138,7 +138,7 @@ public class Animation extends JPanel implements Serializable{
 		else if (num == 4){
 			BufferedImage beach4 = null;
 			try {
-				beach4 = ImageIO.read(new File("images/beach4.png"));
+				beach4 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/beach4.png"));
 			} catch (IOException e) {
 				System.out.println("Failed to load beach, trying bin folder");
 			}
@@ -147,7 +147,7 @@ public class Animation extends JPanel implements Serializable{
 		else {
 			BufferedImage beach5 = null;
 			try {
-				beach5 = ImageIO.read(new File("images/beach5.png"));
+				beach5 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/beach5.png"));
 			} catch (IOException e) {
 				System.out.println("Failed to load beach, trying bin folder");
 			}
@@ -182,7 +182,7 @@ public class Animation extends JPanel implements Serializable{
 	 */
 	public void addHole(int xLoc, int yLoc, int sizeX, int sizeY) {
 		try {
-            BufferedImage hole = ImageIO.read(new File("images/hole2.png"));
+            BufferedImage hole = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/hole2.png"));
 			List<BufferedImage> holeList = new ArrayList<BufferedImage>();
 			holeList.add(hole);
 			AniObject holeObject = new AniObject("hole", xLoc, yLoc, sizeX, sizeY, holeList);
@@ -222,7 +222,7 @@ public class Animation extends JPanel implements Serializable{
 		try {
 			if (plusOrMinus.equalsIgnoreCase("plus")) {
 				if (scoreMult == 1) {
-					BufferedImage plusOne = ImageIO.read(new File("images/horseshoeEgg2.png"));
+					BufferedImage plusOne = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/horseshoeEgg2.png"));
 					List<BufferedImage> scoreList = new ArrayList<BufferedImage>();
 					scoreList.add(plusOne);
 					AniObject scoreObject = new AniObject("plusOne", xLoc, yLoc, sizeX, sizeY, scoreList);
@@ -230,7 +230,7 @@ public class Animation extends JPanel implements Serializable{
 					images.add(scoreObject);
 				}
 				else if (scoreMult == 2) {
-					BufferedImage plusTwo = ImageIO.read(new File("images/doubleEgg2.png"));
+					BufferedImage plusTwo = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/doubleEgg2.png"));
 					List<BufferedImage> scoreList = new ArrayList<BufferedImage>();
 					scoreList.add(plusTwo);
 					AniObject scoreObject = new AniObject("plusTwo", xLoc, yLoc, sizeX, sizeY, scoreList);
@@ -240,7 +240,7 @@ public class Animation extends JPanel implements Serializable{
 				
 			}
 			else if(plusOrMinus.equalsIgnoreCase("minus")) {
-				BufferedImage minusOne = ImageIO.read(new File("images/bottle.png"));
+				BufferedImage minusOne = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/bottle.png"));
 				List<BufferedImage> scoreList = new ArrayList<BufferedImage>();
 				scoreList.add(minusOne);
 				AniObject scoreObject = new AniObject("minusOne", xLoc, yLoc, sizeX, sizeY, scoreList);
@@ -258,7 +258,7 @@ public class Animation extends JPanel implements Serializable{
 	 */
 	public void migrationAnimation() {
 		try {
-			BufferedImage US = ImageIO.read(new File("images/map.png"));
+			BufferedImage US = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/map.png"));
 			List<BufferedImage> USList = new ArrayList<BufferedImage>();
 			USList.add(US);
 			AniObject USObject = new AniObject("US", 350, 100, 450, 600, USList);
@@ -267,9 +267,9 @@ public class Animation extends JPanel implements Serializable{
 			
 			// multiple  images are added to the birdObject's image List.  These
 			// will be painted one after another based on the frame to simulate flapping
-			BufferedImage bird1 = ImageIO.read(new File("images/bird1.png"));
-			BufferedImage bird2 = ImageIO.read(new File("images/bird2.png"));
-			BufferedImage bird3 = ImageIO.read(new File("images/bird3.png"));
+			BufferedImage bird1 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/bird1.png"));
+			BufferedImage bird2 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/bird2.png"));
+			BufferedImage bird3 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/bird3.png"));
 			List<BufferedImage> birdList = new ArrayList<BufferedImage>();
 			birdList.add(bird1);
 			birdList.add(bird2);
@@ -290,7 +290,7 @@ public class Animation extends JPanel implements Serializable{
 	 */
 	public void migrationAnimation2() {
 		try {			
-			BufferedImage US = ImageIO.read(new File("images/map.png"));
+			BufferedImage US = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/map.png"));
 			List<BufferedImage> USList = new ArrayList<BufferedImage>();
 			USList.add(US);
 			AniObject USObject = new AniObject("US", 350, 100, 450, 600, USList);
@@ -326,14 +326,14 @@ public class Animation extends JPanel implements Serializable{
 		BufferedImage egg;
 		BufferedImage nest;
 		try {
-			nest = ImageIO.read(new File("images/nest2.png"));
+			nest = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/nest2.png"));
 			List<BufferedImage> nestList = new ArrayList<BufferedImage>();
 			nestList.add(nest);
 			AniObject nestObject = new AniObject("nest", 475, 450, 300, 200, nestList);
 			nestObject.setVisible(true);
 			images.add(nestObject);
 			
-			egg = ImageIO.read(new File("images/egg2.png"));
+			egg = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/egg2.png"));
 			List<BufferedImage> eggList = new ArrayList<BufferedImage>();
 			eggList.add(egg);
 			AniObject eggObject = new AniObject("egg", 575, 280, 100, 100, eggList);
@@ -383,21 +383,21 @@ public class Animation extends JPanel implements Serializable{
 		try {
 			// only three images are actually used, each inserted 5 times
 			// so that it is painted for 5 frames
-			deadBirdImage1 = ImageIO.read(new File("images/deadbird1.png"));
-			deadBirdImage2 = ImageIO.read(new File("images/deadbird1.png"));
-			deadBirdImage3 = ImageIO.read(new File("images/deadbird1.png"));
-			deadBirdImage4 = ImageIO.read(new File("images/deadbird1.png"));
-			deadBirdImage5 = ImageIO.read(new File("images/deadbird1.png"));
-			deadBirdImage6 = ImageIO.read(new File("images/deadbird2.png"));
-			deadBirdImage7 = ImageIO.read(new File("images/deadbird2.png"));
-			deadBirdImage8 = ImageIO.read(new File("images/deadbird2.png"));
-			deadBirdImage9 = ImageIO.read(new File("images/deadbird2.png"));
-			deadBirdImage10 = ImageIO.read(new File("images/deadbird2.png"));
-			deadBirdImage11 = ImageIO.read(new File("images/deadbird3.png"));
-			deadBirdImage12 = ImageIO.read(new File("images/deadbird3.png"));
-			deadBirdImage13 = ImageIO.read(new File("images/deadbird3.png"));
-			deadBirdImage14 = ImageIO.read(new File("images/deadbird3.png"));
-			deadBirdImage15 = ImageIO.read(new File("images/deadbird3.png"));
+			deadBirdImage1 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird1.png"));
+			deadBirdImage2 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird1.png"));
+			deadBirdImage3 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird1.png"));
+			deadBirdImage4 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird1.png"));
+			deadBirdImage5 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird1.png"));
+			deadBirdImage6 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird2.png"));
+			deadBirdImage7 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird2.png"));
+			deadBirdImage8 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird2.png"));
+			deadBirdImage9 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird2.png"));
+			deadBirdImage10 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird2.png"));
+			deadBirdImage11 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird3.png"));
+			deadBirdImage12 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird3.png"));
+			deadBirdImage13 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird3.png"));
+			deadBirdImage14 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird3.png"));
+			deadBirdImage15 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/deadbird3.png"));
 			List<BufferedImage> deadBirdList = new ArrayList<BufferedImage>();
 			deadBirdList.add(deadBirdImage1);
 			deadBirdList.add(deadBirdImage2);
@@ -431,7 +431,7 @@ public class Animation extends JPanel implements Serializable{
 	public void tombStone() {
 		BufferedImage tomb;
 		try {			
-			tomb = ImageIO.read(new File("images/tomb.png"));
+			tomb = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/tomb.png"));
 			List<BufferedImage> tombList = new ArrayList<BufferedImage>();
 			tombList.add(tomb);
 			AniObject tombObject = new AniObject("tomb", 525, 200, 200, 250, tombList);
@@ -457,12 +457,12 @@ public class Animation extends JPanel implements Serializable{
 			// multiple images added to the maggieObject's image List 
 			// so that each one will be painted one after another for each frame
 			// simulating hand waving
-			BufferedImage maggie1 = ImageIO.read(new File("images/maggie1.png"));
-			BufferedImage maggie2 = ImageIO.read(new File("images/maggie2.png"));
-			BufferedImage maggie3 = ImageIO.read(new File("images/maggie3.png"));
-			BufferedImage maggie4 = ImageIO.read(new File("images/maggie4.png"));
-			BufferedImage maggie5 = ImageIO.read(new File("images/maggie5.png"));
-			BufferedImage maggie6 = ImageIO.read(new File("images/maggie6.png"));
+			BufferedImage maggie1 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/maggie1.png"));
+			BufferedImage maggie2 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/maggie2.png"));
+			BufferedImage maggie3 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/maggie3.png"));
+			BufferedImage maggie4 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/maggie4.png"));
+			BufferedImage maggie5 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/maggie5.png"));
+			BufferedImage maggie6 = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/maggie6.png"));
 			List<BufferedImage> maggieList = new ArrayList<BufferedImage>();
 			maggieList.add(maggie1);
 			maggieList.add(maggie1);
@@ -512,7 +512,7 @@ public class Animation extends JPanel implements Serializable{
 	 */
 	void addQuestionmark(int xLoc, int yLoc, int sizeX, int sizeY) {
 		try {
-			BufferedImage questionmark = ImageIO.read(new File("images/questionMark2.png"));
+			BufferedImage questionmark = ImageIO.read(new File("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/questionMark2.png"));
 			List<BufferedImage> qmList = new ArrayList<BufferedImage>();
 			qmList.add(questionmark);
 			AniObject qmObject = new AniObject("qm", xLoc, yLoc, sizeX, sizeY, qmList);
@@ -545,7 +545,7 @@ public class Animation extends JPanel implements Serializable{
 	 * @return an ImageIcon of the chest
 	 */
 	public ImageIcon getChestIcon(){
-		ImageIcon chest = new ImageIcon("images/chest.png");
+		ImageIcon chest = new ImageIcon("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/chest.png");
 		Image chestImage = chest.getImage();
 		Image resizedChest = chestImage.getScaledInstance( 200, 200,  java.awt.Image.SCALE_SMOOTH ) ;  
 		return new ImageIcon(resizedChest);
@@ -557,7 +557,7 @@ public class Animation extends JPanel implements Serializable{
 	 * @return an ImageIcon of a transparent square
 	 */
 	public ImageIcon getTransChestIcon(){
-		ImageIcon chest = new ImageIcon("images/chestTrans.png");
+		ImageIcon chest = new ImageIcon("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/chestTrans.png");
 		Image chestImage = chest.getImage();
 		Image resizedChest = chestImage.getScaledInstance( 200, 200,  java.awt.Image.SCALE_SMOOTH ) ;  
 		return new ImageIcon(resizedChest);
@@ -568,7 +568,7 @@ public class Animation extends JPanel implements Serializable{
 	 * @return the background image
 	 */
 	public ImageIcon getBackgroundImage() {
-		ImageIcon back = new ImageIcon("images/homeBackground.png");
+		ImageIcon back = new ImageIcon("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/homeBackground.png");
 		Image backImage = back.getImage();
 		Image resizedBack = backImage.getScaledInstance( screenWidth, screenHeight,  java.awt.Image.SCALE_SMOOTH ) ; 
 		ImageIcon newIcon = new ImageIcon(resizedBack);
@@ -580,7 +580,7 @@ public class Animation extends JPanel implements Serializable{
 	 * @return the instructions image
 	 */
 	public ImageIcon getInstructionsIcon() {
-		ImageIcon i = new ImageIcon("images/instructions.png");
+		ImageIcon i = new ImageIcon("D:/Kellie and Billy/Documents/Eclipse/EstuaryProject/EggSweeper/images/instructions.png");
 		Image image = i.getImage();
 		Image newimg = image.getScaledInstance(screenWidth/2, screenHeight/2, Image.SCALE_SMOOTH);
 		return new ImageIcon(newimg);
